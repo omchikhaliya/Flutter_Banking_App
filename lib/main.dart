32f3eef3063bbed51377cc1f13715049ebd3149a
MeetBoda <96529606+MeetBoda@users.noreply.github.com>
2023-09-09 11:03:23 +0530

    Color Updated

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(redColor: Colors.blue),
+      theme: ThemeData(Color: Colors.blue),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
