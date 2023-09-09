9636edefdd439d8c733f214f2a3a4ffaa1d90b4f
{omchikhaliya} <{omchikhaliya121@gmail.com}>
2023-09-09 14:31:04 +0530

    login complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -12,61 +13,80 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
 
+  signInWithEmailAndPassword() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _email.text, password: _password.text);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        return ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("No user found for that email."),
+          ),
+        );
+      } else if (e.code == 'wrong-password') {
+        return ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Wrong password provided for that user"),
+          ),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
         centerTitle: true,
       ),
-      body: (
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Form(
-              key: _formKey,
-              child: OverflowBar(
-                overflowSpacing: 20,
-                children: [
-                  TextFormField(
-                    controller: _email,
-                    validator: (text){
-                      if(text == null || text.isEmpty){
-                        return 'Email is empty';
+      body: (Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Form(
+            key: _formKey,
+            child: OverflowBar(
+              overflowSpacing: 20,
+              children: [
+                TextFormField(
+                  controller: _email,
+                  validator: (text) {
+                    if (text == null || text.isEmpty) {
+                      return 'Email is empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(hintText: "Email"),
+                ),
+                TextFormField(
+                  controller: _password,
+                  validator: (text) {
+                    if (text == null || text.isEmpty) {
+                      return 'password is empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(hintText: "Password"),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        signInWithEmailAndPassword();
+                        print("validation is done");
                       }
-                      return null;
                     },
-                    decoration: const InputDecoration(hintText: "Email"),
+                    child: const Text('Login'),
                   ),
-                  TextFormField(
-                    controller: _password,
-                    validator: (text){
-                      if(text == null || text.isEmpty){
-                        return 'password is empty';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(hintText: "Password"),
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 45,
-                    child: ElevatedButton(
-                      onPressed: (){
-                        if(_formKey.currentState!.validate()){
-                          print("validation is done");
-                        }
-                      },
-                      child: const Text('Login'),
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             ),
           ),
-        )
-      ),
+        ),
+      )),
     );
   }
 }
