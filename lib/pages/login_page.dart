1036d50f26d105d2bd61cd4a54e5b76ae3bd5cf1
Devanshu Chhipani <devanshuchhipani@gmail.com>
2023-09-10 16:34:38 +0530

    feat: Login Page styling

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:banking_application/pages/authentication.dart';
 import 'package:banking_application/pages/home.dart';
+import 'package:flutter/services.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -43,68 +46,119 @@ class _LoginPageState extends State<LoginPage> {
         title: Text('Login'),
         centerTitle: true,
       ),
-      body: (Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: _formKey,
-            child: OverflowBar(
-              overflowSpacing: 20,
-              children: [
-                TextFormField(
-                  controller: _email,
-                  validator: (text) {
-                    if (text == null || text.isEmpty) {
-                      return 'Email is empty';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(hintText: "Email"),
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/images/blue2.jpg'),
+          fit: BoxFit.cover,
+        )),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaY: 50.0, sigmaX: 50.0),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Colors.black,
+                    width: 3,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                TextFormField(
-                  controller: _password,
-                  validator: (text) {
-                    if (text == null || text.isEmpty) {
-                      return 'password is empty';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(hintText: "Password"),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton.icon(
-                      onPressed: () async {
-                        bool auth = await Authentication.authentication();
-                        print("can authenticate: $auth");
-                        if (auth) {
-                          Navigator.pushReplacementNamed(context, '/home');
-                        }
-                      },
-                      icon: Icon(Icons.fingerprint),
-                      label: Text("Authenticate")
+                shadowColor: Colors.black,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Form(
+                    key: _formKey,
+                    child: OverflowBar(
+                      overflowSpacing: 20,
+                      children: [
+                        TextFormField(
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 22.0,
+                          ),
+                          controller: _email,
+                          validator: (text) {
+                            if (text == null || text.isEmpty) {
+                              return 'Email is empty';
+                            }
+                            return null;
+                          },
+                          decoration: const InputDecoration(hintText: "Email"),
+                        ),
+                        TextFormField(
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 22.0,
+                          ),
+                          controller: _password,
+                          validator: (text) {
+                            if (text == null || text.isEmpty) {
+                              return 'password is empty';
+                            }
+                            return null;
+                          },
+                          decoration:
+                              const InputDecoration(hintText: "Password"),
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                            ),
+                              onPressed: () async {
+                                bool auth =
+                                    await Authentication.authentication();
+                                print("can authenticate: $auth");
+                                if (auth) {
+                                  Navigator.pushReplacementNamed(
+                                      context, '/home');
+                                }
+                              },
+                              icon: Icon( Icons.fingerprint),
+                              label: Text("Authenticate",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18.0,
+                                ),
+                              )
+                          ),
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 45,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                            ),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                signInWithEmailAndPassword();
+                                print("validation is done");
+                                Navigator.pushReplacementNamed(
+                                    context, '/home');
+                              }
+                            },
+                            child: const Text('Login',
+                              style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16.0,
+                            ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                SizedBox(
-                  width: double.infinity,
-                  height: 45,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        signInWithEmailAndPassword();
-                        print("validation is done");
-                        Navigator.pushReplacementNamed(context, '/home');
-                      }
-                    },
-                    child: const Text('Login'),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
